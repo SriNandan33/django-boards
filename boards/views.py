678ce7357ea9cb6d00c79f77b0ec9b961e80fc5b
= <=>
2018-10-19 23:23:58 +0530

    add templates n bootstrap4

--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
-
+from django.shortcuts import render
+from .models import Board
 
 def home(request):
-	return HttpResponse('<h1>Hello World</h1>')
+	boards = Board.objects.all()
+	board_names = [board.name for board in boards]
+
+	return render(request, 'boards/home.html', {'boards': boards})
