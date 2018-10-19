59c193ff14b9e7076bcaa282df5ac60567314dae
= <=>
2018-10-19 23:24:10 +0530

    write tests for home view

--- boards/tests.py
+++ boards/tests.py
@@ -1,3 +1,13 @@
+from django.urls import reverse, resolve
 from django.test import TestCase
+from .views import home
 
-# Create your tests here.
+class HomeTests(TestCase):
+	def test_home_view_status_code(self):
+		url = reverse('home')
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 200)
+
+	def test_home_url_resolves_home_view(self):
+		view = resolve('/')
+		self.assertEqual(view.func, home)
