e379fbfa3dcad0bc8a48fbe267c9b877e732e72c
= <=>
2018-10-20 20:29:16 +0530

    complete forms

--- boards/tests.py
+++ boards/tests.py
@@ -3,6 +3,7 @@ from django.urls import reverse, resolve
 from django.test import TestCase
 from .models import Board, Topic, Post
 from .views import home, board_topics, new_topic
+from .forms import NewTopicForm
 
 class HomeTests(TestCase):
 	def setUp(self):
@@ -94,7 +95,9 @@ class newTopicTests(TestCase):
 	def test_new_topic_invalid_post_data(self):
 		url = reverse('new_topic', kwargs={'pk': 1})
 		response = self.client.post(url, {})
+		form = response.context.get('form')
 		self.assertEquals(response.status_code, 200)
+		self.assertTrue(form.errors)
 
 	def test_new_topic_invalid_post_data_empty_fields(self):
 		url = reverse('new_topic', kwargs={'pk': 1})
@@ -106,3 +109,9 @@ class newTopicTests(TestCase):
 		self.assertEquals(response.status_code, 200)
 		self.assertFalse(Topic.objects.exists())
 		self.assertFalse(Post.objects.exists())
+
+	def test_contains_form(self):
+		url = reverse('new_topic', kwargs={'pk':1})
+		response = self.client.get(url)
+		form = response.context.get('form')
+		self.assertIsInstance(form, NewTopicForm)
