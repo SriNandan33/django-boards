d91d2f31bd87aa728bca81df2b862254d64b206a
= <=>
2018-10-21 20:53:54 +0530

    logout and login

--- django_boards/urls.py
+++ django_boards/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path, include
 
 from accounts import views as accounts_views
@@ -21,5 +22,7 @@ from accounts import views as accounts_views
 urlpatterns = [
 	path('', include('boards.urls')),
 	path('signup/', accounts_views.signup, name='signup'),
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
 ]
